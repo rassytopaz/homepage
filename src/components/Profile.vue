014f345fb3d60f29ae45c1aa501d2b7ee31dbeda
rassytopaz <shirotoon8rab@gmail.com>
2019-11-13 15:34:04 +0900

    delete about [news]

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -6,7 +6,7 @@
     <p> {{ text1 }} </p>
     <p> {{ text2 }} </p>
     <p> {{ text3 }} </p>
-    <br><a href="https://www.youtube.com/watch?v=4TK7c3LQHH4">ライブ動画はこちら</a>{{ text4 }}
+    <br><a href="https://www.youtube.com/watch?v=4TK7c3LQHH4">ライブ動画はこちら</a>
   </div>
   </div>
 </template>
@@ -19,7 +19,6 @@ export default {
       text1: '娯楽大学生．友人に誘われたサークルで音楽に没頭し，ボーカルを始める．',
       text2: '主にロックミュージックを中心に，やや高音の曲を担当する事が多い．',
       text3: '趣味は音楽，ゲーム，アニメ，Youtube．',
-      text4: ' (newsに載せる予定)'
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Profile from '@/components/Profile'
-import News from '@/components/More'
+import More from '@/components/More'
 import Link from '@/components/Link'
 
 Vue.use(Router)
@@ -24,7 +24,7 @@ export default new Router({
     {
       path: '/more',
       name: 'More',
-      component: News
+      component: More
     },
 
     //     ②profile
